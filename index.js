5c925fb02c1d067bb777dcd621d0348f8fa1acac
AgusLlave <agusllave9@gmail.com>
2023-09-21 16:25:09 -0300

    big update

--- index.js
+++ index.js
@@ -15,3 +15,56 @@ window.onscroll = () => {
   menu.classList.remove("bx-x");
   navlist.classList.remove("open");
 };
+
+const form = document.getElementById("form");
+
+form.addEventListener("submit", () => {
+  Toastify({
+    text: "Message sent successfully",
+    duration: 3000,
+    style: {
+      background: "linear-gradient(to right, #00b09b, #96c93d)",
+    },
+  }).showToast();
+});
+
+const links = document.querySelectorAll(".nav-link");
+
+if (links.length) {
+  links.forEach((link) => {
+    link.addEventListener("click", () => {
+      links.forEach((link) => {
+        link.classList.remove("active");
+      });
+      link.classList.add("active");
+    });
+  });
+}
+
+const contactBtn = document.getElementsByClassName("btn");
+const watchWorkBtn = document.getElementsByClassName("btn2");
+const homeBtn = document.getElementsByClassName("home-btn");
+const contactDisplay = document.getElementsByClassName("contact-b");
+const portfolioDisplay = document.getElementsByClassName("portfolio-b");
+const homeDisplay = document.getElementsByClassName("home-b");
+
+contactBtn[0].addEventListener("click", () => {
+  links.forEach((link) => {
+    link.classList.remove("active");
+  });
+  contactDisplay[0].classList.add("active");
+});
+
+watchWorkBtn[0].addEventListener("click", () => {
+  links.forEach((link) => {
+    link.classList.remove("active");
+  });
+  portfolioDisplay[0].classList.add("active");
+});
+
+homeBtn[0].addEventListener("click", () => {
+  links.forEach((link) => {
+    link.classList.remove("active");
+  });
+  homeDisplay[0].classList.add("active");
+});
